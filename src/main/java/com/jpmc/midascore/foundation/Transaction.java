c745fd96d0669cdb4bbe1a77599feab1b7c37092
shivammewada-06 <shivammewada17@gmail.com>
2026-01-11 16:48:12 +0530

    task one done

--- src/main/java/com/jpmc/midascore/foundation/Transaction.java
+++ src/main/java/com/jpmc/midascore/foundation/Transaction.java
@@ -2,7 +2,6 @@ package com.jpmc.midascore.foundation;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Transaction {
     private long senderId;
     private long recipientId;
